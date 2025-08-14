b80209e195b51cab074b2f44e053c9c2a5c20d30
vcastrop <vcastrop1@eafit.edu.co>
2025-08-14 08:44:23 -0500

    Third-B Tutorial

--- helloworld/helloworld_project/pages/urls.py
+++ helloworld/helloworld_project/pages/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView
-
+from .views import (HomePageView, AboutPageView,
+                    ContactPageView, ProductIndexView,
+                    ProductShowView, ProductCreateView,
+                    CartView, CartRemoveAllView,
+                    ImageViewFactory, ImageViewNoDI)
+from .interfaces import ImageStorage
+from .utils import ImageLocalStorage
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
@@ -12,4 +17,8 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
     path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenotdi_index'),
+    path('imagenotdi/save', ImageViewNoDI.as_view(), name='imagenotdi_save'),
 ]
--- helloworld/helloworld_project/pages/views.py
+++ helloworld/helloworld_project/pages/views.py
@@ -207,3 +207,32 @@ class CartRemoveAllView(View):
         if 'cart_product_data' in request.session:
             del request.session['cart_product_data']
         return redirect('cart_index')
+
+
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'image/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+
+    return ImageView
+
+class ImageViewNoDI(View):
+    template_name = 'imagenotdi/index.html'
+
+    def get(self, request):
+        image_url = request.session.get('image_url', '')
+        return render(request, self.template_name, {'image_url': image_url})
+
+    def post(self, request):
+        image_storage = ImageLocalStorage()
+        image_url = image_storage.store(request)
+        request.session['image_url'] = image_url
+        return redirect('image_index')
